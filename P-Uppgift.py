a35e87cb4a13da364320592781aa957a50c14177
jacob <jacob@sannes.se>
2025-02-04 03:19:19 +0100

    l

--- P-Uppgift.py
+++ P-Uppgift.py
@@ -20,6 +20,8 @@ class Window():
         self.root.title("P-Uppgift")
         self.root.geometry("300x300")
         self.name_var = tk.StringVar()
+        self.description_var = tk.StringVar()
+        self.context_var = tk.StringVar()
         self.json_data = json_data
 
 
@@ -29,83 +31,124 @@ class Window():
         canvas = tk.Canvas(self.root, width=1024, height=1024)
         canvas.place (relx=0, rely=0, anchor=tk.NW)
         
-        self.search_label = tk.Label(self.root, text="Search for item", font=('calibre', 10, 'bold'), bg='white', fg='black')
-        self.search_label.grid(row=0, column=0)
-        self.search_button = tk.Button(self.root, text='Search', command=self.searchMode, bg='brown', fg='white')
-        self.search_button.grid(row=0, column=1)
+        search_label = tk.Label(self.root, text="Search for item", font=('calibre', 10, 'bold'), bg='white', fg='black')
+        search_label.grid(row=0, column=0)
+        search_button = tk.Button(self.root, text='Search', command=self.searchMode, bg='brown', fg='white')
+        search_button.grid(row=0, column=1)
 
-        self.create_label = tk.Label(self.root, text="Create new item", font=('calibre', 10, 'bold'), bg='white', fg='black')
-        self.create_label.grid(row=1, column=0)
-        self.create_button = tk.Button(self.root, text='Create', command=self.createMode, bg='brown', fg='white')
-        self.create_button.grid(row=1, column=1)
+        create_label = tk.Label(self.root, text="Create new item", font=('calibre', 10, 'bold'), bg='white', fg='black')
+        create_label.grid(row=1, column=0)
+        create_button = tk.Button(self.root, text='Create', command=self.createMode, bg='brown', fg='white')
+        create_button.grid(row=1, column=1)
 
-        self.add_label = tk.Label(self.root, text="Add item to inventory", font=('calibre', 10, 'bold'), bg='white', fg='black')
-        self.add_label.grid(row=2, column=0)
-        self.add_button = tk.Button(self.root, text='Add', command=self.addMode, bg='brown', fg='white')
-        self.add_button.grid(row=2, column=1)
+        add_label = tk.Label(self.root, text="Add item to inventory", font=('calibre', 10, 'bold'), bg='white', fg='black')
+        add_label.grid(row=2, column=0)
+        add_button = tk.Button(self.root, text='Add', command=self.addMode, bg='brown', fg='white')
+        add_button.grid(row=2, column=1)
 
-        self.edit_label = tk.Label(self.root, text="Edit item", font=('calibre', 10, 'bold'), bg='white', fg='black')
-        self.edit_label.grid(row=3, column=0)
-        self.edit_button = tk.Button(self.root, text='Edit', command=self.editMode, bg='brown', fg='white')
-        self.edit_button.grid(row=3, column=1)
+        edit_label = tk.Label(self.root, text="Edit item", font=('calibre', 10, 'bold'), bg='white', fg='black')
+        edit_label.grid(row=3, column=0)
+        edit_button = tk.Button(self.root, text='Edit', command=self.editMode, bg='brown', fg='white')
+        edit_button.grid(row=3, column=1)
 
-        self.remove_label = tk.Label(self.root, text="Remove item", font=('calibre', 10, 'bold'), bg='white', fg='black')
-        self.remove_label.grid(row=4, column=0)
-        self.remove_button = tk.Button(self.root, text='Remove', command=lambda: self.hide(self.remove_label), bg='brown', fg='white')
-        self.remove_button.grid(row=4, column=1)
+        remove_label = tk.Label(self.root, text="Remove item", font=('calibre', 10, 'bold'), bg='white', fg='black')
+        remove_label.grid(row=4, column=0)
+        remove_button = tk.Button(self.root, text='Remove', command=self.removeMode, bg='brown', fg='white')
+        remove_button.grid(row=4, column=1)
 
         self.mainMenu = [
-            [self.search_label, self.search_button],
-            [self.create_label, self.create_button],
-            [self.add_label, self.add_button],
-            [self.edit_label, self.edit_button],
-            [self.remove_label, self.remove_button]
+            search_label, search_button,
+            create_label, create_button,
+            add_label, add_button,
+            edit_label, edit_button,
+            remove_label, remove_button
         ]
         self.currentMode = self.mainMenu
-        self.entry_label = tk.Label(self.root, text = "Search for item",font = ('calibre',10,'bold'), bg = 'white', fg = 'black')
-        self.entry = tk.Entry(self.root, textvariable = self.name_var ,font = ('calibre',10,'bold'), bg = 'white', fg = 'black')      
-        #entry_label.grid(row=3, column=0)
-        #entry.grid(row=3, column=1)
-        self.sub_btn=tk.Button(self.root,text = 'Search', command = self.search, bg='brown', fg='white')
-        self.back_btn=tk.Button(self.root,text = 'Back', command = self.mainMenuMode, bg='brown', fg='white')
-        self.search_buttons = [[self.entry_label, self.entry], [self.sub_btn], [self.back_btn]]
+
+
+
+        entry_label = tk.Label(self.root, text = "Search for item",font = ('calibre',10,'bold'), bg = 'white', fg = 'black')
+        entry = tk.Entry(self.root, textvariable = self.name_var ,font = ('calibre',10,'bold'), bg = 'white', fg = 'black')      
+        entry_label.grid(row=3, column=0)
+        entry.grid(row=3, column=1)
+        sub_btn=tk.Button(self.root,text = 'Search', command = self.search, bg='brown', fg='white')
+        back_btn=tk.Button(self.root,text = 'Back', command = self.mainMenuMode, bg='brown', fg='white')
+        self.search_buttons = [entry_label, entry, sub_btn, back_btn]
+        self.hide(self.search_buttons)
+
+
+        #create Mode
+        create_label = tk.Label(self.root, text="Create new item", font=('calibre', 10, 'bold'), bg='white', fg='black')
+        create_label.grid(row=3, column=0)
+
+        name_label = tk.Label(self.root, text="Name", font=('calibre', 10, 'bold'), bg='white', fg='black')
+        name_label.grid(row=4, column=0)
+        create_name_entry = tk.Entry(self.root, textvariable=self.name_var, font=('calibre', 10, 'bold'), bg='white', fg='black')
+        create_name_entry.grid(row=4, column=1)
+
+        description_label = tk.Label(self.root, text="Description", font=('calibre', 10, 'bold'), bg='white', fg='black')
+        description_label.grid(row=5, column=0)
+        create_description_entry = tk.Entry(self.root, textvariable=self.description_var, font=('calibre', 10, 'bold'), bg='white', fg='black')
+        create_description_entry.grid(row=5, column=1)
+
+        context_label = tk.Label(self.root, text="Context", font=('calibre', 10, 'bold'), bg='white', fg='black')
+        context_label.grid(row=6, column=0)
+        create_context_entry = tk.Entry(self.root, textvariable=self.context_var, font=('calibre', 10, 'bold'), bg='white', fg='black')
+        create_context_entry.grid(row=6, column=1)
+
+        create_sub_btn = tk.Button(self.root, text='Create', command=self.create, bg='brown', fg='white')
+        create_sub_btn.grid(row=7, column=1)
+        self.create_mode = [create_label, name_label, create_name_entry, description_label, create_description_entry, context_label, create_context_entry, create_sub_btn]
+        self.hide(self.create_mode)
         self.root.mainloop()
+
+
+        #edit Mode
+
         
         #sub_btn.grid(row=4, column=1)
         #self.root.mainloop()
     
-    def hide(self, widget):
-        widget.grid_remove()
+    def hide(self, widgets):
+        for widget in widgets:
+            widget.grid_remove()
 
-    def show(self, widget):
-        widget.grid()
+    def show(self, widgets):
+        for widget in widgets:
+            widget.grid()
 
     def mainMenuMode(self):
-        for widget in self.currentMode:
-            for w in widget:
-                self.hide(w)
-        for widget in self.mainMenu:
-            for w in widget:
-                self.show(w)
+        self.hide(self.currentMode)
+        self.show(self.mainMenu)
+        self.currentMode = self.mainMenu
         
 
     def searchMode(self):
-        for widget in self.mainMenu:
-            for w in widget:
-                self.hide(w)
-        for widget in self.search_buttons:
-            for w in widget:
-                self.show(w)
+        self.hide(self.mainMenu)
+        self.show(self.search_buttons)
         self.currentMode = self.search_buttons
         pass
 
     def createMode(self):
-        for widget in self.mainMenu:
-            for w in widget:
-                self.hide(w)
-
+        self.hide(self.mainMenu)
+        self.show(self.create_mode)
+        self.currentMode = self.create_mode
+        print (self.json_data)
         pass
 
+    def create(self):
+
+        new_item = {
+            "name": self.name_var.get(),
+            "description": self.description_var.get().split(","),
+            "context": self.context_var.get(),
+            "id": len(self.json_data['items']) + 1
+        }
+        self.name_var.set("")
+        self.description_var.set("")
+        self.context_var.set("")
+        self.json_data['items'].append(new_item)
+        self.mainMenuMode()
     def addMode(self):
         pass
 
